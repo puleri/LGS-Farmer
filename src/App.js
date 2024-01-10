456b40837308b57de4dd9ea88507fbb6e19898f5
puleri <puleri.mp4@gmail.com>
2024-01-10 15:05:57 -0800

    another test page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import TestPage from './components/Test/Test'
+import TestPage2 from './components/Test2/Test2'
+
 
 
 function App() {
@@ -10,9 +12,10 @@ function App() {
 
   return (
     <Router>
-    <Routes>
-      <Route exact path="/" element={<TestPage/>}/>
-    </Routes>
+      <Routes>
+        <Route exact path="/" element={<TestPage/>}/>
+        <Route exact path="/test2" element={<TestPage2/>}/>
+      </Routes>
     </Router>
   );
 }
---
+++ src/components/Test2/Test2.js
@@ -0,0 +1,17 @@
+
+function TestPage2() {
+
+    return (
+        <>
+            <div>
+                <h1>
+                    hi World
+                </h1>
+            </div>
+        </>
+    );
+}
+
+export default TestPage2;
+
+
